459d2094d56fae61ddf9ad1b5b7078b5cc38bb80
ifty03 <ashikulislamb84@gmail.com>
2022-10-05 18:26:52 +0600

    first commit

---
+++ output/script.js
@@ -0,0 +1,12 @@
+"use strict";
+const book = "English";
+const shop = ["lala", "khala", "pala"];
+let nothing;
+let arr;
+arr = {
+    name: "ashik",
+    age: 23,
+    adult: true
+};
+nothing = ["how", 3, false];
+console.log(nothing);
---
+++ src/script.ts
@@ -0,0 +1,17 @@
+const book: string = "English"
+const shop: string[]=["lala","khala","pala"];
+let nothing: (string | number | boolean)[];
+
+let arr:{
+    name:string,
+    age:number,
+    adult:boolean
+};
+arr={
+    name:"ashik",
+    age:23,
+    adult:true
+}
+
+nothing=["how",3,false];
+console.log(nothing);
